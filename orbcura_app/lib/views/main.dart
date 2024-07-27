f461b0e1396d4441db5875b6da495ed8fb528808
Parag Sharma <sharmaparag2004@gmail.com>
2024-07-27 14:33:16 +0530

    splash updated

--- orbcura_app/lib/views/main.dart
+++ orbcura_app/lib/views/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: splashscreen(),
+      home: SplashScreen(),
     );
   }
 }
