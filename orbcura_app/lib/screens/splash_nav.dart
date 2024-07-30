53597a9d570e34ecc029bd3320194d5432eec644
Parag Sharma <sharmaparag2004@gmail.com>
2024-07-30 23:17:12 +0530

    update

--- orbcura_app/lib/screens/splash_nav.dart
+++ orbcura_app/lib/screens/splash_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:orbcura_app/widgets/four_corner_screen.dart';
 
 class SplashNavScreen extends StatefulWidget {
@@ -12,56 +13,66 @@ class _SplashNavScreenState extends State<SplashNavScreen> {
   @override
   Widget build(BuildContext context) {
     var h = MediaQuery.sizeOf(context).height;
-    var w = MediaQuery.sizeOf(context).height;
+    var w = MediaQuery.sizeOf(context).width;
 
     return FourCornerScreen(
-        CornerChild(
+      CornerChild(
+        Image.asset(
+          "assets/mic.png",
+          height: h / 16,
+        ),
+        () {},
+      ),
+      CornerChild(
+        Image.asset(
+          "assets/communicate.png",
+          height: h / 16,
+        ),
+        () {},
+      ),
+      CornerChild(
+        Image.asset(
+          "assets/home.png",
+          height: h / 16,
+        ),
+        () {},
+      ),
+      CornerChild(
+        Image.asset(
+          "assets/next.png",
+          height: h / 16,
+        ),
+        () {},
+      ),
+      Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
             Image.asset(
-              "assets/mic.png",
-              height: h / 16,
+              'assets/background_splash_nav.png', // Replace with your image asset path
+              fit: BoxFit.cover,
             ),
-            () {}),
-        CornerChild(
-          Image.asset(
-            "assets/communicate.png",
-            height: h / 16,
-          ),
-          () {},
-        ),
-        CornerChild(
-          Image.asset(
-            "assets/home.png",
-            height: h / 16,
-          ),
-          () {},
-        ),
-        CornerChild(
-          Image.asset(
-            "assets/next.png",
-            height: h / 16,
-          ),
-          () {},
-        ),
-        Scaffold(
-          body: Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              Image.asset(
-                'assets/background_splash_nav.png', // Replace with your image asset path
-                fit: BoxFit.cover,
-              ),
-              Center(
-                child: Text(
-                  'Welcome to the Splash Navigation Screen!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Spacer(flex: 7),
+                Image.asset(
+                  'assets/app_logo.png', // Ensure the path is correct
+                  width: w / 2,
+                  height: h / 4,
                 ),
-              ),
-            ],
-          ),
-        ));
+                SizedBox(height: 20), // Add some space between images
+                Image.asset(
+                  'assets/upi_button.png', // Path to your new image
+                  width: w / 2,
+                  height: h / 1.5,
+                ),
+                Spacer(flex: 100),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
