930c3d4b8f4ace13580bc23441d6add982eeb4e8
Parag Sharma <sharmaparag2004@gmail.com>
2024-07-31 21:51:14 +0530

    update

--- orbcura_app/test/widget_test.dart
+++ orbcura_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:orbcura_app/views/main.dart';
+import 'package:orbcura_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
