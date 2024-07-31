d047f33f72137bf06f7fb1eb651d8f442bbdbc55
Hemish <hemish04082005@gmail.com>
2024-07-31 20:59:07 +0530

    Made Login Screen

---
+++ orbcura_app/lib/screens/login.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:orbcura_app/widgets/four_corner_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var h = MediaQuery.sizeOf(context).height;
+
+    return FourCornerScreen(
+        CornerChild(
+            Image.asset(
+              "assets/mic.png",
+              height: h / 16,
+            ),
+            () {}),
+        CornerChild(
+          Image.asset(
+            "assets/communicate.png",
+            height: h / 16,
+          ),
+          () {},
+        ),
+        CornerChild(
+          Image.asset(
+            "assets/account.png",
+            height: h / 16,
+          ),
+          () {},
+        ),
+        CornerChild(
+          Image.asset(
+            "assets/login.png",
+            height: h / 16,
+          ),
+          () {},
+        ),
+
+        Center(child: Container(decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/login_background.png"), fit: BoxFit.fill)),),)
+        
+    );
+  }
+}
